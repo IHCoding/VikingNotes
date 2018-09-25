e5b55bad895577626c6e545b24596932894bcd93
IHCoding <s.imal@yahoo.com>
2018-09-25 10:04:10 +0200

    Added Search field to the design. layout.cshtml

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-namespace VikingNotes.Controllers
+namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
